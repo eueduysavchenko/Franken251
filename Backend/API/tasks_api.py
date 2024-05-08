fbad275ed7ad99c9b189ca679155c4bef412a633
vovachub <166488979+vovachub@users.noreply.github.com>
2024-05-08 12:17:48 +0300

    Add tasks feature by chubenko (#1)
    * First commit
    
    * Add gitignore file
    
    * Separate add new user and users sections
    
    * Add flex style to sections
    
    * Replace Student model by task
    
    * Replace Student model by task 2
    
    * Add Delete task button
    
    * Implement Deleting task
    
    * Add Edit task button with moving data to form
    
    * Refactor handlers and add Cancel button
    
    * Add editing task
    
    * Add form validation and button disabling
    
    * Update naming
    
    * Return students files back and update tasks route
    
    * Update delete/update endpoints
    
    * Add back to api list link
    
    * Update routes for delete and update endpoints
    
    * Delete .idea/Franken251.iml
    
    * Delete .idea/misc.xml
    
    ---------
    
    Co-authored-by: Volodymyr <volodymyr.chubenko@sisense.com>
    Co-authored-by: eueduysavchenko <165005308+eueduysavchenko@users.noreply.github.com>

---
+++ Backend/API/tasks_api.py
@@ -0,0 +1,93 @@
+from flask import Blueprint, jsonify, request
+from Models.task_model import Task
+
+tasks_api = Blueprint('tasks_api', __name__)
+
+
+@tasks_api.route('/tasks', methods=['GET'])
+def get_tasks():
+    tasks = [task for task in Task.select()]
+    return jsonify([{
+        'id': task.id,
+        'person': task.person,
+        'title': task.title,
+        'description': task.description,
+        'status': task.status
+    }
+        for task in tasks
+    ])
+
+@tasks_api.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    try:
+        task = Task.get_by_id(task_id)
+        return jsonify({
+            'id': task.id,
+            'person': task.person,
+            'title': task.title,
+            'description': task.description,
+            'status': task.status
+        })
+    except Task.DoesNotExist:
+        return jsonify({'success': False, 'message': 'Task not found'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@tasks_api.route('/tasks', methods=['POST'])
+def add_task():
+    data = request.get_json()
+    #  Перевіряємо чи переданий ID, це ключове поле яке не може бути пустим
+    task_id = int(data.get('id')) if 'id' in data and data['id'] else None
+
+    if task_id:
+        # Спроба знайти існуючого студента
+        task = Task.get_or_none(Task.id == task_id)
+        if task:
+            # Оновлення існуючого студента
+            task.person = data['person']
+            task.title = data['title']
+            task.description = data['description']
+            task.status = data['status']
+            task.save()
+        else:
+            # Якщо завдання не знайдений, створюємо нового з вказаним ID
+            task = Task.create(id=task_id, **data)
+    else:
+        # Видалення 'id' з даних, щоб уникнути конфліктів з NULL значеннями
+        data.pop('id', None)
+        # Створення нового завдання без вказання ID
+        task = Task.create(**data)
+
+    return jsonify({
+        'id': task.id, 'person': task.person, 'title': task.title,
+        'description': task.description, 'status': task.status
+    }), 200 if task_id else 201
+
+@tasks_api.route('/tasks/<task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    try:
+        task = Task.get(Task.id == task_id)
+        task.delete_instance()
+        return jsonify({'success': True, 'message': 'Task deleted successfully'})
+    except Task.DoesNotExist:
+        return jsonify({'success': False, 'message': 'Task not found'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@tasks_api.route('/tasks/<int:task_id>', methods=['PATCH'])
+def update_task(task_id):
+    try:
+        data = request.get_json()
+        task = Task.get(Task.id == task_id)
+        task.person = data.get('person', task.person)
+        task.title = data.get('title', task.title)
+        task.description = data.get('description', task.description)
+        task.status = data.get('status', task.status)
+
+        task.save()
+        return jsonify({'success': True, 'message': 'Task updated successfully'})
+    except Task.DoesNotExist:
+        return jsonify({'success': False, 'message': 'Task not found'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
--- Backend/app.py
+++ Backend/app.py
@@ -6,6 +6,10 @@ import os
 from Models.students_model import initialize_students_db
 from API.students_api import students_api as students_api
 from API.students_route import students_route
+# Імпорт необхідних файлів для Tasks
+from Models.task_model import initialize_tasks_db
+from API.tasks_api import tasks_api as tasks_api
+from API.tasks_route import tasks_route
 
 # Ініціалізація app та шляху до статичних даних
 app = Flask(__name__, static_folder='../Frontend')
@@ -15,10 +19,13 @@ CORS(app)
 
 # Ініціалізація бази даних
 initialize_students_db()
+initialize_tasks_db()
 
 # Реєстрація Blueprints для API та Навігції FrontEnd в браузері
 app.register_blueprint(students_api, url_prefix='/api')
 app.register_blueprint(students_route)
+app.register_blueprint(tasks_api, url_prefix='/api')
+app.register_blueprint(tasks_route)
 
 
 # Точка входу, index.html
@@ -35,16 +42,23 @@ def send_js(path):
         # Забороняємо доступ до шляху, якщо він містить небезпечні символи
         abort(404)
 
-    # Формуємо абсолютний шлях до файлу для безпечного доступу
-    safe_path = os.path.abspath(os.path.join(students_route.static_folder, path))
+    if path.startswith('/students'):
+        # Формуємо абсолютний шлях до файлу для безпечного доступу
+        safe_path = os.path.abspath(os.path.join(students_route.static_folder, path))
+        # Перевіряємо, чи знаходиться файл дійсно у дозволеній директорії
+        if not safe_path.startswith(os.path.abspath(students_route.static_folder)):
+            abort(404)
+        # Відправляємо файл за безпечним шляхом
+        return send_from_directory(students_route.static_folder, path)
 
-    # Перевіряємо, чи знаходиться файл дійсно у дозволеній директорії
-    if not safe_path.startswith(os.path.abspath(students_route.static_folder)):
-        abort(404)
-
-    # Відправляємо файл за безпечним шляхом
-    return send_from_directory(students_route.static_folder, path)
-
+    if path.startswith('/tasks'):
+        # Формуємо абсолютний шлях до файлу для безпечного доступу
+        safe_path = os.path.abspath(os.path.join(tasks_route.static_folder, path))
+        # Перевіряємо, чи знаходиться файл дійсно у дозволеній директорії
+        if not safe_path.startswith(os.path.abspath(tasks_route.static_folder)):
+            abort(404)
+        # Відправляємо файл за безпечним шляхом
+        return send_from_directory(tasks_route.static_folder, path)
 
 # Запуск сервера в режимі тестування
 if __name__ == '__main__':
