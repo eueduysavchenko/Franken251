3a0ad66b3aba2369c5931b9e6317c5ef4ff8a32b
AZavodnoi <a.zavodnoy@e-u.edu.ua>
2024-05-09 13:29:38 +0300

    first commit

--- Frontend/students/students.js
+++ Frontend/students/students.js
@@ -1,4 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
+    function deleteStudent(studentId){
+        // Відправляємо запит DELETE на сервер для видалення студента за його ID
+        fetch(`/api/students/${studentId}`, {
+            method: 'DELETE'
+        })
+        .then(response => response.json())
+        .then(() => {
+            // Оновлюємо список студентів на сторінці
+            loadStudents();
+        })
+        .catch(error => console.error('Error:', error));
+    }
+
+    function updateStudent(data){
+        const loader = document.getElementById('loader');    
+
+        fetch(`/api/students`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)           
+        })
+        .then(response => response.json())
+        .then(() => {
+            // Оновлюємо список студентів на сторінці
+            loadStudents();            
+
+            // Змінюємо колір кнопки на зелений на 1500 мілісекунди
+            const updateButton = document.querySelector(`.update-${data.id}`);
+            updateButton.style.backgroundColor = 'lightgreen';
+            setTimeout(() => {
+                updateButton.style.backgroundColor = ''; // Скидаємо колір на стандартний
+            }, 1500);
+
+        })
+        .catch(error => console.error('Error:', error));
+    }
+    
     // Функція для завантаження та відображення списку студентів
     function loadStudents() {
         fetch('/api/students')
@@ -9,13 +48,45 @@ document.addEventListener('DOMContentLoaded', function() {
                 studentsList.innerHTML = '';
                 students.forEach(student => {
                     const row = studentsList.insertRow();
+                
+                console.log(row);
                     row.innerHTML = `
                         <td>${student.id}</td>
-                        <td>${student.group}</td>
-                        <td>${student.first_name}</td>
-                        <td>${student.last_name}</td>
-                        <td>${student.score}</td>
+                        <td><input class="group" value=${student.group}></td>
+                        <td><input class="first_name" value=${student.first_name}></td>
+                        <td><input class="last_name" value=${student.last_name}></td>
+                        <td><input class="score" value=${student.score}></td>
+                        <td>
+                            <input class="delete-${student.id}" type="button" value="Delete" data-student-id="${student.id}">
+                            <input class="update-${student.id}" type="button" value="Update" data-student-id="${student.id}">                            
+                        </td>
                     `;
+                     // Додаємо обробник події click до кнопки Delete                    
+                    const deleteButton = row.querySelector(`.delete-${student.id}`);
+                    deleteButton.addEventListener('click', function() {
+                        const studentId = this.getAttribute('data-student-id');
+                        deleteStudent(studentId);
+                    });
+
+                    // Додаємо обробник події click до кнопки Update
+                    const updateButton = row.querySelector(`.update-${student.id}`);
+                    updateButton.addEventListener('click', function(event) {
+                        const id = this.getAttribute('data-student-id');
+                        const group = event.target.parentNode.parentNode.querySelector('.group').value;
+                        const firstName = event.target.parentNode.parentNode.querySelector('.first_name').value;
+                        const lastName = event.target.parentNode.parentNode.querySelector('.last_name').value;
+                        const score = event.target.parentNode.parentNode.querySelector('.score').value;
+                        
+                        const updatedStudent = {
+                            id: id,
+                            group: group,
+                            first_name: firstName,
+                            last_name: lastName,
+                            score: score                
+                        };                    
+
+                        updateStudent(updatedStudent);                        
+                    });
                 });
             });
     }
