a084194e7cdd6223a33451723e36a9984eeb6ecd
eueduysavchenko <165005308+eueduysavchenko@users.noreply.github.com>
2024-06-13 09:34:11 +0300

    Revert "TEACHER API: added GET/POST implementation"

--- Backend/app.py
+++ Backend/app.py
@@ -2,12 +2,6 @@ from flask import Flask, send_from_directory, abort
 from flask_cors import CORS
 import os
 
-<<<<<<< HEAD
-# Імпорт необхідних файлів для teachers
-from Models.teachers_model import initialize_teachers_db
-from API.teachers_api import teachers_api
-from API.teachers_route import teachers_route
-=======
 # Імпорт необхідних файлів для Students
 from Models.students_model import initialize_students_db
 from API.students_api import students_api as students_api
@@ -24,7 +18,6 @@ from API.library_route import library_route
 from Models.movies_model import initialize_movies_db
 from API.movies_api import movies_api as movies_api
 from API.movies_route import movies_route
->>>>>>> main
 
 # Ініціалізація app та шляху до статичних даних
 app = Flask(__name__, static_folder='../Frontend')
@@ -33,13 +26,6 @@ app = Flask(__name__, static_folder='../Frontend')
 CORS(app)
 
 # Ініціалізація бази даних
-<<<<<<< HEAD
-initialize_teachers_db()
-
-# Реєстрація Blueprints для API та Навігції FrontEnd в браузері
-app.register_blueprint(teachers_api, url_prefix='/api')
-app.register_blueprint(teachers_route)
-=======
 initialize_students_db()
 initialize_tasks_db()
 initialize_library_db()
@@ -54,7 +40,6 @@ app.register_blueprint(library_api, url_prefix='/api')
 app.register_blueprint(library_route)
 app.register_blueprint(movies_api, url_prefix='/api')
 app.register_blueprint(movies_route)
->>>>>>> main
 
 
 # Точка входу, index.html
@@ -71,17 +56,6 @@ def send_js(path):
         # Забороняємо доступ до шляху, якщо він містить небезпечні символи
         abort(404)
 
-<<<<<<< HEAD
-    # Формуємо абсолютний шлях до файлу для безпечного доступу
-    safe_path = os.path.abspath(os.path.join(teachers_route.static_folder, path))
-
-    # Перевіряємо, чи знаходиться файл дійсно у дозволеній директорії
-    if not safe_path.startswith(os.path.abspath(teachers_route.static_folder)):
-        abort(404)
-
-    # Відправляємо файл за безпечним шляхом
-    return send_from_directory(teachers_route.static_folder, path)
-=======
     if path.startswith('/students'):
         # Формуємо абсолютний шлях до файлу для безпечного доступу
         safe_path = os.path.abspath(os.path.join(students_route.static_folder, path))
@@ -115,9 +89,8 @@ def send_js(path):
             abort(404)
 
         return send_from_directory(movies_route.static_folder, path)
->>>>>>> main
 
 
 # Запуск сервера в режимі тестування
 if __name__ == '__main__':
-    app.run(debug=True, port=8082)
+    app.run(debug=True, port=8080)
